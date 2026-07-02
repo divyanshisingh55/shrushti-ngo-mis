da31b8aa28c7c21f301f29b631df319de2e9e218
divyanshisingh55 <singhdivyanshi55@gmail.com>
2026-07-02 14:19:50 +0530

    Update API URL fallback config

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+console.log("API URL =", import.meta.env.VITE_API_URL);
+
 export default axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "http://localhost:5000",
 });
